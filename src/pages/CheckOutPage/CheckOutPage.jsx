c950b53717fde2ceade19079a10ba4610dfcb1c4
Afrosiyob <afrosiyob.a97@gmail.com>
2020-10-09 13:14:52 +0500

    yup
    yup

--- src/pages/CheckOutPage/CheckOutPage.jsx
+++ src/pages/CheckOutPage/CheckOutPage.jsx
@@ -2,64 +2,51 @@ import { MDBCol, MDBContainer, MDBRow } from "mdbreact";
 import React from "react";
 import "./CheckOutPage.scss";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { MDBAnimation } from "mdbreact";
+// import { MDBAnimation } from "mdbreact";
 import * as Yup from "yup";
 
 const initialValues = {
-  billingDetails: {
-    firstName: "",
-    lastName: "",
-    companyName: "",
-    country: "",
-    streetAddress: "",
-    aptCode: "",
-    city: "",
-    postcode: "",
-    state: "",
-    email: "",
-    phone: "",
-    createAcc: "",
-  },
-  shippingDetails: {
-    createAcc: "",
-    orderNote: "",
-  },
-  order: {
-    products: {
-      productName: "adidas",
-      productCount: "10",
-      productPrice: "100$",
-    },
-    subtotal: "1000$",
-    shipping: "20$",
-    total: "1120$",
-    payment: "",
-    agree: "",
-  },
+  firstName: "",
+  lastName: "",
+  companyName: "",
+  country: "",
+  streetAddress: "",
+  aptSuit: "",
+  city: "",
+  postcodeZip: "",
+  state: "",
+  email: "",
+  phone: "",
+  createAnAccount: false,
+  orderNotes: "",
+  productName: "adidas",
+  ProductPrice: "300$",
+  productCount: 5,
+  subtotal: 1500,
+  shipping: 300,
+  total: 1800,
+  payment: [],
+  readAgree: false,
 };
 
 const validationSchema = Yup.object().shape({
-  billingDetails: Yup.object().shape({
-    firstName: Yup.string().required("Required."),
-    lastName: Yup.string().required("Required."),
+  firstName: Yup.string().required("Required !"),
+  lastName: Yup.string().required("Required !"),
 
-    country: Yup.string().required("Required."),
-    streetAddress: Yup.string().required("Required."),
-    aptCode: Yup.string().required("Required."),
-    city: Yup.string().required("Required."),
-    postcode: Yup.string().required("Required."),
-    state: Yup.string().required("Required."),
-    email: Yup.string().email("Invalid email").required("Required"),
-    phone: Yup.string().required("Required"),
-    createAcc: Yup.string().email("Invalid email").required("Required"),
-  }),
-  shippingDetails: Yup.object().shape({
-    createAcc: Yup.string().required("Required."),
-  }),
-  order: Yup.object().shape({
-    payment: Yup.string().required("Required!"),
-    agree: Yup.string().required("Required"),
-  }),
+  country: Yup.string().required("Required !"),
+  streetAddress: Yup.string().required("Required !"),
+  aptSuit: Yup.string().required("Required !"),
+  city: Yup.string().required("Required !"),
+  postcodeZip: Yup.string().required("Required !"),
+  state: Yup.string().required("Required !"),
+  email: Yup.string().email("Invalid email").required("Required !"),
+  phone: Yup.string()
+    .min(7, "Too short!")
+    .max(20, "Too Long!")
+    .required("Required !"),
+  createAnAccount: Yup.string().required("Required !"),
+  // payment: Yup.string().required("Required !"),
+  // readAgree: Yup.string().required("Required !"),
 });
 
 function CheckOutPage() {
@@ -69,13 +56,13 @@ function CheckOutPage() {
         <h1>Your selected products</h1>
         <Formik
           initialValues={initialValues}
+          validationSchema={validationSchema}
           onSubmit={(values, actions) => {
             setTimeout(() => {
               alert(JSON.stringify(values, null, 2));
               actions.setSubmitting(false);
             }, 1000);
           }}
-          validationSchema={validationSchema}
         >
           {({ errors, touched, values }) => (
             <Form>
@@ -87,16 +74,20 @@ function CheckOutPage() {
                   <MDBRow>
                     <MDBCol md="6" sm="12">
                       <div className="mt-3">
+                        <label htmlFor="firstName">
+                          <small>
+                            <b>First Name</b>
+                          </small>
+                        </label>
                         <Field
-                          class="form-control rounded"
+                          className="form-control"
                           type="text"
-                          id="billingDetails.firstName"
-                          name="billingDetails.firstName"
-                          placeholder="enter first name"
-                          required
+                          id="firstName"
+                          name="firstName"
+                          placeholder="First Name"
                         />
                         <small style={{ color: "red" }}>
-                          <ErrorMessage name="billingDetails.firstName">
+                          <ErrorMessage name="firstName">
                             {(msg) => <div>{msg}</div>}
                           </ErrorMessage>
                         </small>
@@ -104,16 +95,20 @@ function CheckOutPage() {
                     </MDBCol>
                     <MDBCol md="6" sm="12">
                       <div className="mt-3">
+                        <label htmlFor="lastName">
+                          <small>
+                            <b>Last Name</b>
+                          </small>
+                        </label>
                         <Field
-                          class="form-control rounded"
+                          className="form-control"
                           type="text"
-                          id="billingDetails.lastName"
-                          name="billingDetails.lastName"
-                          placeholder="enter last name"
-                          required
+                          id="lastName"
+                          name="lastName"
+                          placeholder="Last Name"
                         />
                         <small style={{ color: "red" }}>
-                          <ErrorMessage name="billingDetails.lastName">
+                          <ErrorMessage name="lastName">
                             {(msg) => <div>{msg}</div>}
                           </ErrorMessage>
                         </small>
@@ -124,35 +119,38 @@ function CheckOutPage() {
                   <MDBRow>
                     <MDBCol md="12">
                       <div className="mt-3">
+                        <label htmlFor="companyName">
+                          <small>
+                            <b> Company Name (Optional)</b>
+                          </small>
+                        </label>
                         <Field
-                          class="form-control rounded"
+                          className="form-control"
                           type="text"
-                          id="billingDetails.companyName"
-                          name="billingDetails.companyName"
-                          placeholder="enter company name"
-                          required
+                          id="companyName"
+                          name="companyName"
+                          placeholder="Company Name"
                         />
-                        <small style={{ color: "red" }}>
-                          <ErrorMessage name="billingDetails.companyName">
-                            {(msg) => <div>{msg}</div>}
-                          </ErrorMessage>
-                        </small>
                       </div>
                     </MDBCol>
                   </MDBRow>
                   <MDBRow>
                     <MDBCol md="12">
                       <div className="mt-3">
+                        <label htmlFor="country">
+                          <small>
+                            <b>Country</b>
+                          </small>
+                        </label>
                         <Field
-                          class="form-control rounded"
+                          className="form-control"
                           type="text"
-                          id="billingDetails.country"
-                          name="billingDetails.country"
-                          placeholder="enter country name"
-                          required
+                          id="country"
+                          name="country"
+                          placeholder="Country"
                         />
                         <small style={{ color: "red" }}>
-                          <ErrorMessage name="billingDetails.country">
+                          <ErrorMessage name="country">
                             {(msg) => <div>{msg}</div>}
                           </ErrorMessage>
                         </small>
@@ -162,49 +160,229 @@ function CheckOutPage() {
                   <MDBRow>
                     <MDBCol md="8" sm="12">
                       <div className="mt-3">
+                        <label htmlFor="streetAddress">
+                          <small>
+                            <b>Street Address</b>
+                          </small>
+                        </label>
                         <Field
-                          class="form-control rounded"
+                          className="form-control"
                           type="text"
-                          id="billingDetails.streetAddress"
-                          name="billingDetails.streetAddress"
-                          placeholder="enter first streetAddress"
-                          required
+                          id="streetAddress"
+                          name="streetAddress"
+                          placeholder="streetAddress"
                         />
                         <small style={{ color: "red" }}>
-                          <ErrorMessage name="billingDetails.streetAddress">
+                          <ErrorMessage name="streetAddress">
                             {(msg) => <div>{msg}</div>}
                           </ErrorMessage>
                         </small>
                       </div>
                     </MDBCol>
                     <MDBCol md="4" sm="12">
-                      test
+                      <div className="mt-3">
+                        <label htmlFor="aptSuit">
+                          <small>
+                            <b>citypostcodeZip</b>
+                          </small>
+                        </label>
+                        <Field
+                          className="form-control"
+                          type="text"
+                          id="aptSuit"
+                          name="aptSuit"
+                          placeholder="aptSuit"
+                        />
+                        <small style={{ color: "red" }}>
+                          <ErrorMessage name="aptSuit">
+                            {(msg) => <div>{msg}</div>}
+                          </ErrorMessage>
+                        </small>
+                      </div>
                     </MDBCol>
                   </MDBRow>
                   <MDBRow>
                     <MDBCol md="5" sm="12">
-                      test
+                      <div className="mt-3">
+                        {/* <label htmlFor=""></label>
+                        <Field
+                          className="form-control"
+                          type=""
+                          id=""
+                          name=""
+                          placeholder=""
+                        />
+                        <small style={{ color: "red" }}>
+                          <ErrorMessage name="">
+                            {(msg) => <div>{msg}</div>}
+                          </ErrorMessage>
+                        </small> */}
+
+                        <label htmlFor="city">
+                          <small>
+                            <b>citypostcodeZip</b>
+                          </small>
+                        </label>
+                        <Field
+                          className="form-control"
+                          type="text"
+                          id="city"
+                          name="city"
+                          placeholder="city"
+                        />
+                        <small style={{ color: "red" }}>
+                          <ErrorMessage name="city">
+                            {(msg) => <div>{msg}</div>}
+                          </ErrorMessage>
+                        </small>
+                      </div>
                     </MDBCol>
                     <MDBCol md="7" sm="12">
-                      test
+                      <div className="mt-3">
+                        {" "}
+                        <label htmlFor="postcodeZip">
+                          <small>
+                            <b>postcodeZip</b>
+                          </small>
+                        </label>
+                        <Field
+                          className="form-control"
+                          type="text"
+                          id="postcodeZip"
+                          name="postcodeZip"
+                          placeholder="postcodeZip"
+                        />
+                        <small style={{ color: "red" }}>
+                          <ErrorMessage name="postcodeZip">
+                            {(msg) => <div>{msg}</div>}
+                          </ErrorMessage>
+                        </small>
+                      </div>
                     </MDBCol>
                   </MDBRow>
                   <MDBRow>
-                    <MDBCol md="12">test</MDBCol>
+                    <MDBCol md="12">
+                      <div className="mt-3">
+                        {" "}
+                        <label htmlFor="state">
+                          <small>
+                            <b>state</b>
+                          </small>
+                        </label>
+                        <Field
+                          className="form-control"
+                          type="texxt"
+                          id="state"
+                          name="state"
+                          placeholder="state"
+                        />
+                        <small style={{ color: "red" }}>
+                          <ErrorMessage name="state">
+                            {(msg) => <div>{msg}</div>}
+                          </ErrorMessage>
+                        </small>
+                      </div>
+                    </MDBCol>
                   </MDBRow>
                   <MDBRow>
                     <MDBCol md="6" sm="12">
-                      test
+                      <div className="mt-3">
+                        {" "}
+                        <label htmlFor="email">
+                          <small>
+                            <b>email</b>
+                          </small>
+                        </label>
+                        <Field
+                          className="form-control"
+                          type="email"
+                          id="email"
+                          name="email"
+                          placeholder="email"
+                        />
+                        <small style={{ color: "red" }}>
+                          <ErrorMessage name="email">
+                            {(msg) => <div>{msg}</div>}
+                          </ErrorMessage>
+                        </small>
+                      </div>
                     </MDBCol>
                     <MDBCol md="6" sm="12">
-                      test
+                      <div className="mt-3">
+                        {" "}
+                        <label htmlFor="phone">
+                          <small>
+                            <b>phone</b>
+                          </small>
+                        </label>
+                        <Field
+                          className="form-control"
+                          type="phone"
+                          id="phone"
+                          name="phone"
+                          placeholder="phone"
+                        />
+                        <small style={{ color: "red" }}>
+                          <ErrorMessage name="phone">
+                            {(msg) => <div>{msg}</div>}
+                          </ErrorMessage>
+                        </small>
+                      </div>
                     </MDBCol>
                   </MDBRow>
+
+                  <MDBCol md="12">
+                    <div className="mt-3">
+                      <Field
+                        type="checkbox"
+                        id="createAnAccount"
+                        name="createAnAccount"
+                      />
+                      <label className="ml-3" htmlFor="createAnAccount">
+                        <div className="d-flex flex-column">
+                          <p className="m-0 p-0">
+                            <b>Create An Account</b>
+                          </p>
+                          <small>
+                            * If you select this option, an account will be
+                            opened for you based on the information you entered
+                            above.
+                          </small>
+                        </div>
+                      </label>
+                    </div>
+                  </MDBCol>
+                  <div className="my-3">
+                    <hr />
+                  </div>
+
+                  <h3 className="text-uppercase font-weight-bold">
+                    Shipping Details details
+                  </h3>
+
+                  <MDBCol>
+                    <div className="mt-3">
+                      <Field
+                        type="checkbox"
+                        id="createAnAccount"
+                        name="createAnAccount"
+                      />
+                      <label className="ml-3" htmlFor="createAnAccount">
+                        <div className="d-flex flex-column">
+                          <p className="m-0 p-0">
+                            <b>Create An Account</b>
+                          </p>
+                        </div>
+                      </label>
+                    </div>
+                  </MDBCol>
                 </MDBCol>
                 <MDBCol md="4" sm="12">
                   right-item
                 </MDBCol>
               </MDBRow>
+
+              <button type="submit">Submit</button>
             </Form>
           )}
         </Formik>
