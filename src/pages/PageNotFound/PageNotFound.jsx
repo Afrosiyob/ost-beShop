37a1455002f2a240ece27bb91bb5f4a601c20b38
Afrosiyob <afrosiyob.a97@gmail.com>
2020-10-12 23:12:22 +0500

    add
    addd

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,22 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import "./PageNotFound.scss";
-function PageNotFound() {
-  const [stateX, setstateX] = useState(0);
-  const [stateY, setstateY] = useState(0);
 
-  const handleChange = (e) => {
-    setstateX(e.nativeEvent.offsetX);
-    setstateY(e.nativeEvent.offsetY);
-  };
+import $ from "jquery";
+
+import ImgNumber from "../../assets/404page/404.svg";
+
+function PageNotFound() {
+  useEffect(() => {
+    $(".not-found-page").mousemove(function (e) {
+      console.log("====================================");
+      console.log("x = " + e.offsetX + " :::: y = " + e.offsetY);
+      console.log("====================================");
+      console.log("====================================");
+      console.log(
+        "widht = " +
+          e.target.offsetWidth +
+          " :::: heigth = " +
+          e.target.offsetHeight
+      );
+      console.log("====================================");
+
+      $(".img-number").css({
+        left:
+          e.offsetX > e.target.offsetWidth
+            ? 50 + e.offsetX / 300 + "%"
+            : 50 - e.offsetX / 300 + "%",
+        top:
+          e.offsetY > e.target.offsetHeight
+            ? 50 + e.offsetY / 100 + "%"
+            : 50 - e.offsetY / 100 + "%",
+      });
+    });
+  }, []);
 
   return (
-    <div
-      className="not-found-page"
-      onMouseMove={(e) => {
-        handleChange(e);
-      }}
-    >
-      this is page not found :::: {stateX} and {stateY}
+    <div className="not-found-page">
+      <div className="images-wrap-box">
+        <img src={ImgNumber} className="img-number" alt="number" />
+      </div>
     </div>
   );
 }
